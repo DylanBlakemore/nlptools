0dabf6f8fa8a09f99f21d98179f38212f2050b63
Dylan Blakemore <dylan.blakemore@gmail.com>
2022-01-27 00:11:58 +0200

    Initial commit

---
+++ src/main/java/com/simple_nlg_server/Realizer.java
@@ -0,0 +1,61 @@
+package com.simple_nlg_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+
+// These need to be imported as is. The realiser converts XML
+// into objects, so the classes need to all be loaded.
+import simplenlg.aggregation.*;
+import simplenlg.features.*;
+import simplenlg.format.english.*;
+import simplenlg.framework.*;
+import simplenlg.lexicon.*;
+import simplenlg.morphology.english.*;
+import simplenlg.orthography.english.*;
+import simplenlg.phrasespec.*;
+import simplenlg.realiser.english.*;
+import simplenlg.syntax.english.*;
+
+import simplenlg.xmlrealiser.wrapper.RecordSet;
+import simplenlg.xmlrealiser.XMLRealiser;
+import simplenlg.xmlrealiser.XMLRealiserException;
+import simplenlg.xmlrealiser.wrapper.DocumentRealisation;
+
+import java.io.StringReader;
+
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class Realizer {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Realizer.class, args);
+  }
+
+  @PostMapping("/realize")
+  public String realize(@RequestBody Map<String, Object> xmlMap) {
+    String xml = xmlMap.get("xml").toString();
+
+    RecordSet records = null;
+
+    try(StringReader reader = new StringReader(xml)) {
+			records = XMLRealiser.getRecording(reader);
+      String paragraph = "";
+      for (DocumentRealisation document : records.getRecord()) {
+        paragraph = paragraph + document.getRealisation().trim();
+      }
+
+      return paragraph;
+		} catch(XMLRealiserException e) {
+      throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Unable to parse XML into text", e);
+		} catch (Exception e) {
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "An error occurred", e);
+    }
+  }
+}
---
+++ src/test/java/com/simple_nlg_server/RealizerTests.java
@@ -0,0 +1,48 @@
+package com.simple_nlg_server;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RealizerTests {
+
+  @Autowired
+	private MockMvc mockMvc;
+
+  private String getXml() throws IOException {
+    String xml = new String(Files.readAllBytes(Paths.get("./src/test/java/com/simple_nlg_server/test_xml.txt")));
+    return xml;
+  }
+  
+  @Test
+	public void shouldReturnDefaultMessage() throws Exception {
+    String xml = getXml().trim();
+    String content = String.format("{\"xml\": \"%s\"}", xml);
+
+		this.mockMvc
+        .perform(
+          post("/realize")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(content))
+        .andDo(print())
+        .andExpect(status().isOk())
+				.andExpect(content().string(containsString("An angioplasty balloon catheter, the D701000000992, was deployed.")));
+	}
+
+}
